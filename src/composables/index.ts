ffec9a313e768c64e1791cc7568d3e5e077096cf
everleandro <ever.leandros91@gmail.com>
2024-01-07 11:50:07 -0600

    expose composables on index

---
+++ src/composables/index.ts
@@ -0,0 +1,2 @@
+export { default as UseBreakpoint } from "./breakpoint";
+export { default as useField } from "./field";
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export * from "./components";
+export * from "./composables";
 export * from "./directives";
 export * from "./types";
+export { default as DateBuilder } from "./utils/date";
 import "@/assets/sass/main.scss";
 
 export { default as Drocket } from "./framework";
